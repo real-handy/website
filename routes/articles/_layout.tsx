5c40f9a3142a3d9c9d187469a0bd6eec8d11195e
Sean Silvius <sean@silvius.me>
2024-03-19 22:29:04 -0700

    fixed fix

--- routes/articles/_layout.tsx
+++ routes/articles/_layout.tsx
@@ -1,6 +1,5 @@
 import { FreshContext } from "$fresh/server.ts"
 import Header from "../../islands/Header.tsx" 
-import Footer from "../../components/Footer.tsx"
 
 export default async function Layout(req: Request, ctx: FreshContext) {
 
